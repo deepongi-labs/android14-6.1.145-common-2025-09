fb0f47ed929e5330c6056137b033d2b7119566f0
Sebastian Ene <sebastianene@google.com>
2025-08-26 13:00:32 +0000

    ANDROID: KVM: Prevent memory sharing outside of the RAM regions
    Verify the address to be shared and prevent MMIO page
    sharing. Allow MMIO to be donated to the hypervisor and
    to the guest for device assignment.
    
    Bug: 439996285
    Signed-off-by: Sebastian Ene <sebastianene@google.com>
    
    Cleaned up host_check_page_state_flags.
    
    Change-Id: I98590534865d3239911350736831b8564d614fef
    Signed-off-by: Keir Fraser <keirf@google.com>
    (cherry picked from commit 33eb6bde43d03bd826214bbb390de62ca19621b9)
    [Lee: Fixed-up various merge conflicts]
    Signed-off-by: Lee Jones <joneslee@google.com>

--- arch/arm64/kvm/hyp/nvhe/mem_protect.c
+++ arch/arm64/kvm/hyp/nvhe/mem_protect.c
@@ -988,10 +988,14 @@ static enum pkvm_page_state host_get_mmio_page_state(kvm_pte_t pte, u64 addr)
 	return state | pkvm_getstate(prot);
 }
 
+enum host_check_page_state_flags {
+	HOST_CHECK_NULL_REFCNT		= BIT(0),
+	HOST_CHECK_IS_MEMORY		= BIT(1),
+};
+
 static int ___host_check_page_state_range(u64 addr, u64 size,
 					  enum pkvm_page_state state,
-					  struct memblock_region *reg,
-					  bool check_null_refcount)
+					  enum host_check_page_state_flags flags)
 {
 	struct check_walk_data d = {
 		.desired	= state,
@@ -999,6 +1003,16 @@ static int ___host_check_page_state_range(u64 addr, u64 size,
 	};
 	u64 end = addr + size;
 	struct hyp_page *p;
+	struct memblock_region *reg;
+	struct kvm_mem_range range;
+
+	/* Can't check the state of both MMIO and memory regions at once */
+	reg = find_mem_range(addr, &range);
+	if (!reg && (flags & HOST_CHECK_IS_MEMORY))
+		return -EINVAL;
+
+	if (!is_in_mem_range(end - 1, &range))
+		return -EINVAL;
 
 	hyp_assert_lock_held(&host_mmu.lock);
 
@@ -1013,7 +1027,7 @@ static int ___host_check_page_state_range(u64 addr, u64 size,
 		p = hyp_phys_to_page(addr);
 		if (p->host_state != state)
 			return -EPERM;
-		if (check_null_refcount && hyp_refcount_get(p->refcount))
+		if ((flags & HOST_CHECK_NULL_REFCNT) && hyp_refcount_get(p->refcount))
 			return -EINVAL;
 	}
 
@@ -1029,17 +1043,13 @@ static int ___host_check_page_state_range(u64 addr, u64 size,
 static int __host_check_page_state_range(u64 addr, u64 size,
 					 enum pkvm_page_state state)
 {
-	struct memblock_region *reg;
-	struct kvm_mem_range range;
-	u64 end = addr + size;
+	enum host_check_page_state_flags flags = HOST_CHECK_IS_MEMORY;
 
-	/* Can't check the state of both MMIO and memory regions at once */
-	reg = find_mem_range(addr, &range);
-	if (!is_in_mem_range(end - 1, &range))
-		return -EINVAL;
+	if (state == PKVM_PAGE_OWNED)
+		flags |= HOST_CHECK_NULL_REFCNT;
 
 	/* Check the refcount of PAGE_OWNED pages as those may be used for DMA. */
-	return ___host_check_page_state_range(addr, size, state, reg, state == PKVM_PAGE_OWNED);
+	return ___host_check_page_state_range(addr, size, state, flags);
 }
 
 static int __host_set_page_state_range(u64 addr, u64 size,
@@ -1582,7 +1592,7 @@ int __pkvm_host_donate_hyp_locked(u64 pfn, u64 nr_pages, enum kvm_pgtable_prot p
 
 	hyp_lock_component();
 
-	ret = __host_check_page_state_range(phys, size, PKVM_PAGE_OWNED);
+	ret = ___host_check_page_state_range(phys, size, PKVM_PAGE_OWNED, HOST_CHECK_NULL_REFCNT);
 	if (ret)
 		goto unlock;
 	if (IS_ENABLED(CONFIG_NVHE_EL2_DEBUG)) {
@@ -1623,7 +1633,7 @@ int __pkvm_hyp_donate_host(u64 pfn, u64 nr_pages)
 	if (ret)
 		goto unlock;
 	if (IS_ENABLED(CONFIG_NVHE_EL2_DEBUG)) {
-		ret = __host_check_page_state_range(phys, size, PKVM_NOPAGE);
+		ret = ___host_check_page_state_range(phys, size, PKVM_NOPAGE, 0);
 		if (ret)
 			goto unlock;
 	}
@@ -1691,8 +1701,8 @@ int module_change_host_page_prot(u64 pfn, enum kvm_pgtable_prot prot, u64 nr_pag
 		}
 	} else {
 		/* The entire range must be pristine. */
-		ret = ___host_check_page_state_range(
-				addr, nr_pages << PAGE_SHIFT, PKVM_PAGE_OWNED, reg, true);
+		ret = ___host_check_page_state_range(addr, nr_pages << PAGE_SHIFT,
+						     PKVM_PAGE_OWNED, HOST_CHECK_NULL_REFCNT);
 		if (ret)
 			goto unlock;
 	}
@@ -1889,12 +1899,12 @@ static int __pkvm_use_dma_locked(phys_addr_t phys_addr, size_t size,
 
 		ret = ___host_check_page_state_range(phys_addr, size,
 						     PKVM_PAGE_TAINTED,
-						     reg, false);
+						     0);
 		if (!ret)
 			return ret;
 		ret = ___host_check_page_state_range(phys_addr, size,
 						     PKVM_PAGE_OWNED,
-						     reg, false);
+						     0);
 		if (ret)
 			return ret;
 		prot = pkvm_mkstate(PKVM_HOST_MMIO_PROT, PKVM_PAGE_TAINTED);
@@ -2233,7 +2243,7 @@ int __pkvm_host_donate_guest(u64 pfn, u64 gfn, struct pkvm_hyp_vcpu *vcpu, u64 n
 	host_lock_component();
 	guest_lock_component(vm);
 
-	ret = __host_check_page_state_range(phys, size, PKVM_PAGE_OWNED);
+	ret = ___host_check_page_state_range(phys, size, PKVM_PAGE_OWNED, HOST_CHECK_NULL_REFCNT);
 	if (ret)
 		goto unlock;
 	ret = __guest_check_page_state_range(vcpu, ipa, size, PKVM_NOPAGE);
@@ -2317,7 +2327,8 @@ int __pkvm_host_donate_sglist_guest(struct pkvm_hyp_vcpu *vcpu)
 			goto unlock;
 		}
 
-		ret = __host_check_page_state_range(phys, size, PKVM_PAGE_OWNED);
+		ret = ___host_check_page_state_range(phys, size, PKVM_PAGE_OWNED,
+						     HOST_CHECK_NULL_REFCNT);
 		if (ret)
 			goto unlock;
 
@@ -2384,7 +2395,8 @@ int __pkvm_host_donate_sglist_hyp(struct pkvm_sglist_page *sglist, size_t nr_pag
 			goto unlock;
 		}
 
-		ret = __host_check_page_state_range(phys, size, PKVM_PAGE_OWNED);
+		ret = ___host_check_page_state_range(phys, size, PKVM_PAGE_OWNED,
+						     HOST_CHECK_NULL_REFCNT);
 		if (ret)
 			goto unlock;
 
@@ -2496,7 +2508,8 @@ int __pkvm_host_reclaim_page(struct pkvm_hyp_vm *vm, u64 pfn, u64 ipa, u8 order)
 
 	switch ((int)guest_get_page_state(pte, ipa)) {
 	case PKVM_PAGE_OWNED:
-		WARN_ON(__host_check_page_state_range(phys, page_size, PKVM_NOPAGE));
+		WARN_ON(___host_check_page_state_range(phys, page_size, PKVM_NOPAGE,
+						       HOST_CHECK_IS_MEMORY));
 		/* No vCPUs of the guest can run, doing this prior to stage-2 unmap is OK */
 		hyp_poison_page(phys, page_size);
 		psci_mem_protect_dec(1 << order);
