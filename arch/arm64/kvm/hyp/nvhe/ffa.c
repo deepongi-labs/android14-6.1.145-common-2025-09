BayLibre/android_kernel_common

ANDROID: KVM: arm64: Use SMCCC 1.2 to notify vm availability
Change-Id: I9b7aea75063f53b48070b22e3565b6526fd992c7
Signed-off-by: Per Larsen <perlarsen@google.com>

arch/arm64/kvm/hyp/nvhe/ffa.c
@@ -395,7 +395,7 @@ static int kvm_notify_vm_availability(uint16_t vm_handle, struct kvm_ffa_buffers
 				      u32 availability_msg)
 {
 	int i;
-	struct arm_smccc_res res;
+	struct arm_smccc_1_2_regs regs;
 	u64 avail_bit = availability_msg != FFA_VM_DESTRUCTION_MSG;
 
 	for (i = 0; i < num_vm_avail_sps; i++) {
@@ -432,10 +432,16 @@ static int kvm_notify_vm_availability(uint16_t vm_handle, struct kvm_ffa_buffers
 		 * Some TEEs return NOT_SUPPORTED instead.
 		 * If that happens, ignore the error and continue.
 		 */
-		arm_smccc_1_1_smc(FFA_RUN, dest, 0, 0, 0, 0, 0, 0, &res);
-		if (res.a0 == FFA_ERROR && (int)res.a2 != FFA_RET_NOT_SUPPORTED)
-			return ffa_to_linux_errno(res.a2);
-		else if (res.a0 == FFA_INTERRUPT)
+		regs = (struct arm_smccc_1_2_regs) {
+			.a0 = FFA_RUN,
+			.a1 = dest,
+		};
+		__hyp_exit();
+		arm_smccc_1_2_smc(&regs, &regs);
+		__hyp_enter();
+		if (regs.a0 == FFA_ERROR && (int)regs.a2 != FFA_RET_NOT_SUPPORTED)
+			return ffa_to_linux_errno(regs.a2);
+		else if (regs.a0 == FFA_INTERRUPT)
 			return -EINTR;
 
 		if (availability_msg == FFA_VM_DESTRUCTION_MSG &&
@@ -451,28 +457,42 @@ static int kvm_notify_vm_availability(uint16_t vm_handle, struct kvm_ffa_buffers
 			 * we will never retry the creation message, and the SP
 			 * will probably leak its state for the pending VM.
 			 */
-			arm_smccc_1_1_smc(FFA_MSG_SEND_DIRECT_REQ, vm_avail_sps[i].sp_id,
-					  FFA_VM_CREATION_MSG, HANDLE_LOW(FFA_INVALID_HANDLE),
-					  HANDLE_HIGH(FFA_INVALID_HANDLE), vm_handle, 0, 0,
-					  &res);
+			regs = (struct arm_smccc_1_2_regs) {
+				.a0 = FFA_MSG_SEND_DIRECT_REQ,
+				.a1 = vm_avail_sps[i].sp_id,
+				.a2 = FFA_VM_CREATION_MSG,
+				.a3 = HANDLE_LOW(FFA_INVALID_HANDLE),
+				.a4 = HANDLE_HIGH(FFA_INVALID_HANDLE),
+				.a5 = vm_handle,
+			};
+			__hyp_exit();
+			arm_smccc_1_2_smc(&regs, &regs);
+			__hyp_exit();
 
-			if (res.a0 != FFA_MSG_SEND_DIRECT_RESP)
+			if (regs.a0 != FFA_MSG_SEND_DIRECT_RESP)
 				return -EINVAL;
-			if (res.a3 != FFA_RET_SUCCESS)
-				return ffa_to_linux_errno(res.a3);
+			if (regs.a3 != FFA_RET_SUCCESS)
+				return ffa_to_linux_errno(regs.a3);
 
 			/* Creation completed successfully, clear the flag */
 			ffa_buf->vm_creating_bitmap &= ~sp_mask;
 		}
 
-		arm_smccc_1_1_smc(FFA_MSG_SEND_DIRECT_REQ, vm_avail_sps[i].sp_id,
-				  availability_msg, HANDLE_LOW(FFA_INVALID_HANDLE),
-				  HANDLE_HIGH(FFA_INVALID_HANDLE), vm_handle, 0, 0,
-				  &res);
-		if (res.a0 != FFA_MSG_SEND_DIRECT_RESP)
+		regs = (struct arm_smccc_1_2_regs) {
+				.a0 = FFA_MSG_SEND_DIRECT_REQ,
+				.a1 = vm_avail_sps[i].sp_id,
+				.a2 = availability_msg,
+				.a3 = HANDLE_LOW(FFA_INVALID_HANDLE),
+				.a4 = HANDLE_HIGH(FFA_INVALID_HANDLE),
+				.a5 = vm_handle,
+			};
+		__hyp_exit();
+		arm_smccc_1_2_smc(&regs, &regs);
+		__hyp_enter();
+		if (regs.a0 != FFA_MSG_SEND_DIRECT_RESP)
 			return -EINVAL;
 
-		switch ((int)res.a3) {
+		switch ((int)regs.a3) {
 		case FFA_RET_SUCCESS:
 			ffa_buf->vm_avail_bitmap &= ~sp_mask;
 			ffa_buf->vm_avail_bitmap |= avail_value;
@@ -486,7 +506,7 @@ static int kvm_notify_vm_availability(uint16_t vm_handle, struct kvm_ffa_buffers
 
 			fallthrough;
 		default:
-			return ffa_to_linux_errno(res.a3);
+			return ffa_to_linux_errno(regs.a3);
 		}
 	}
 
@@ -1520,7 +1540,7 @@ static void do_ffa_direct_msg(struct kvm_cpu_context *ctxt,
 static int kvm_host_ffa_signal_availability(void)
 {
 	int ret;
-	struct arm_smccc_res res;
+	struct arm_smccc_1_2_regs res;
 
 	/*
 	 * Map our hypervisor buffers into the SPMD before mapping and

deepongi-labs/android14-6.1.145-common-2025-09

Bug: 280886201

arch/arm64/kvm/hyp/nvhe/ffa.c
@@ -391,7 +391,7 @@ static int kvm_notify_vm_availability(uint16_t vm_handle, struct kvm_ffa_buffers
 				      u32 availability_msg)
 {
 	int i;
-	struct arm_smccc_res res;
+	struct arm_smccc_1_2_regs res;
 	u64 avail_bit = availability_msg != FFA_VM_DESTRUCTION_MSG;
 
 	for (i = 0; i < num_vm_avail_sps; i++) {
@@ -428,7 +428,13 @@ static int kvm_notify_vm_availability(uint16_t vm_handle, struct kvm_ffa_buffers
 		 * Some TEEs return NOT_SUPPORTED instead.
 		 * If that happens, ignore the error and continue.
 		 */
-		arm_smccc_1_1_smc(FFA_RUN, dest, 0, 0, 0, 0, 0, 0, &res);
+		res = (struct arm_smccc_1_2_regs) {
+			.a0 = FFA_RUN,
+			.a1 = dest,
+		};
+		__hyp_exit();
+		arm_smccc_1_2_smc(&res, &res);
+		__hyp_enter();
 		if (res.a0 == FFA_ERROR && (int)res.a2 != FFA_RET_NOT_SUPPORTED)
 			return ffa_to_linux_errno(res.a2);
 		else if (res.a0 == FFA_INTERRUPT)
@@ -447,10 +453,17 @@ static int kvm_notify_vm_availability(uint16_t vm_handle, struct kvm_ffa_buffers
 			 * we will never retry the creation message, and the SP
 			 * will probably leak its state for the pending VM.
 			 */
-			arm_smccc_1_1_smc(FFA_MSG_SEND_DIRECT_REQ, vm_avail_sps[i].sp_id,
-					  FFA_VM_CREATION_MSG, HANDLE_LOW(FFA_INVALID_HANDLE),
-					  HANDLE_HIGH(FFA_INVALID_HANDLE), vm_handle, 0, 0,
-					  &res);
+			res = (struct arm_smccc_1_2_regs) {
+				.a0 = FFA_MSG_SEND_DIRECT_REQ,
+				.a1 = vm_avail_sps[i].sp_id,
+				.a2 = FFA_VM_CREATION_MSG,
+				.a3 = HANDLE_LOW(FFA_INVALID_HANDLE),
+				.a4 = HANDLE_HIGH(FFA_INVALID_HANDLE),
+				.a5 = vm_handle,
+			};
+			__hyp_exit();
+			arm_smccc_1_2_smc(&res, &res);
+			__hyp_enter();
 
 			if (res.a0 != FFA_MSG_SEND_DIRECT_RESP)
 				return -EINVAL;
@@ -461,10 +474,17 @@ static int kvm_notify_vm_availability(uint16_t vm_handle, struct kvm_ffa_buffers
 			ffa_buf->vm_creating_bitmap &= ~sp_mask;
 		}
 
-		arm_smccc_1_1_smc(FFA_MSG_SEND_DIRECT_REQ, vm_avail_sps[i].sp_id,
-				  availability_msg, HANDLE_LOW(FFA_INVALID_HANDLE),
-				  HANDLE_HIGH(FFA_INVALID_HANDLE), vm_handle, 0, 0,
-				  &res);
+		res = (struct arm_smccc_1_2_regs) {
+				.a0 = FFA_MSG_SEND_DIRECT_REQ,
+				.a1 = vm_avail_sps[i].sp_id,
+				.a2 = availability_msg,
+				.a3 = HANDLE_LOW(FFA_INVALID_HANDLE),
+				.a4 = HANDLE_HIGH(FFA_INVALID_HANDLE),
+				.a5 = vm_handle,
+			};
+		__hyp_exit();
+		arm_smccc_1_2_smc(&res, &res);
+		__hyp_enter();
 		if (res.a0 != FFA_MSG_SEND_DIRECT_RESP)
 			return -EINVAL;
 