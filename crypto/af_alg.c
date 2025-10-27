deepongi-labs/android14-6.1.145-common-2025-09

commit 3522ea727e6ecd3b88462771ccb692ae7577b332

UPSTREAM: crypto: af_alg - Disallow concurrent writes in af_alg_sendmsg
commit 1b34cbbf4f011a121ef7b2d7d6e6920a036d5285 upstream.

Issuing two writes to the same af_alg socket is bogus as the
data will be interleaved in an unpredictable fashion.  Furthermore,
concurrent writes may create inconsistencies in the internal
socket state.

Disallow this by adding a new ctx->write field that indiciates
exclusive ownership for writing.

Bug: 446278751
Fixes: 8ff590903d5 ("crypto: algif_skcipher - User-space interface for skcipher operations")
Reported-by: Muhammad Alifa Ramdhan <ramdhan@starlabs.sg>
Reported-by: Bing-Jhong Billy Jheng <billy@starlabs.sg>
Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>
Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
(cherry picked from commit 9aee87da5572b3a14075f501752e209801160d3d)
Signed-off-by: Lee Jones <joneslee@google.com>
Change-Id: Ie4b99c532ab77a6e25483ece1f42ed4c4744e70f

crypto/af_alg.c
@@ -862,6 +862,12 @@ int af_alg_sendmsg(struct socket *sock, struct msghdr *msg, size_t size,
 	}
 
 	lock_sock(sk);
+	if (ctx->write) {
+		release_sock(sk);
+		return -EBUSY;
+	}
+	ctx->write = true;
+
 	if (ctx->init && !ctx->more) {
 		if (ctx->used) {
 			err = -EINVAL;
@@ -969,6 +975,7 @@ int af_alg_sendmsg(struct socket *sock, struct msghdr *msg, size_t size,
 
 unlock:
 	af_alg_data_wakeup(sk);
+	ctx->write = false;
 	release_sock(sk);
 
 	return copied ?: err;

include/crypto/if_alg.h
@@ -136,6 +136,7 @@ struct af_alg_async_req {
  *			SG?
  * @enc:		Cryptographic operation to be performed when
  *			recvmsg is invoked.
+ * @write:		True if we are in the middle of a write.
  * @init:		True if metadata has been sent.
  * @len:		Length of memory allocated for this data structure.
  * @inflight:		Non-zero when AIO requests are in flight.
@@ -151,10 +152,11 @@ struct af_alg_ctx {
 	size_t used;
 	atomic_t rcvused;
 
-	bool more;
-	bool merge;
-	bool enc;
-	bool init;
+	u32		more:1,
+			merge:1,
+			enc:1,
+			write:1,
+			init:1;
 
 	unsigned int len;
 

commit 789a7fc63e60a90ee73d500da16e4c59fbdc2815

ANDROID: crypto: af_alg - Fix assignment from bitfield
A recent security patch in af_alg changed converted bool members of
af_alg_ctx into bitfields. Update assignment to the bitfield to prevent
the following kernelci error:

crypto/af_alg.c: In function ‘af_alg_sendpage’:
crypto/af_alg.c:1033:21: error: overflow in conversion from ‘int’ to ‘unsigned char:1’ changes value from ‘flags & 32768’ to ‘0’ [-Werror=overflow]
 1033 |         ctx->more = flags & MSG_MORE;
      |                     ^~~~~
This line is no longer present in upstream as of commit dc97391e6610
("sock: Remove ->sendpage*() in favour of sendmsg(MSG_SPLICE_PAGES)"),
which was introduced after 6.1.

Bug: 454240277
Fixes: 3522ea727e6e ("UPSTREAM: crypto: af_alg - Disallow concurrent writes in af_alg_sendmsg")
Change-Id: I5f1a944aeb44eb7a9b181924ef8b82fe79771353
Signed-off-by: Tiffany Yang <ynaffit@google.com>

crypto/af_alg.c
@@ -1035,7 +1035,7 @@ ssize_t af_alg_sendpage(struct socket *sock, struct page *page,
 	ctx->used += size;
 
 done:
-	ctx->more = flags & MSG_MORE;
+	ctx->more = !!(flags & MSG_MORE);
 
 unlock:
 	af_alg_data_wakeup(sk);