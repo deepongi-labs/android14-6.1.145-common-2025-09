BayLibre/android_kernel_common

ANDROID: vendor_hooks: Add hooks for dmv
By these vhs, we can obtain the informations when dmv corrupt happend
such as 4k want_digest, runtime_digest, 4k block is compared, etc.
- android_vh_handle_add_fec_mismatch_blks

- android_vh_handle_error

- android_vh_handle_add_skipped_blks

- android_vh_handle_add_blks_map

- android_vh_handle_get_b_info

Bug: 439737442

Change-Id: Ibb33e6bbca93ae37b0a992edbae126778702953a
Signed-off-by: hoai.tt5 <hoai.tt5@samsung.corp-partner.google.com>

drivers/android/vendor_hooks.c
@@ -77,6 +77,7 @@
 #include <trace/hooks/user.h>
 #include <trace/hooks/gzvm.h>
 #include <trace/hooks/usb.h>
+#include <trace/hooks/dmv_debug.h>
 
 /*
  * Export tracepoints that act as a bare tracehook (ie: have no trace event
@@ -616,3 +617,8 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mm_remove_migration_pte_bypass);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mm_split_huge_page_bypass);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mm_try_split_folio_bypass);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_scmi_limit_notify_cb);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_handle_add_fec_mismatch_blks);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_handle_error);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_handle_add_skipped_blks);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_handle_add_blks_map);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_handle_get_b_info);

deepongi-labs/android14-6.1.145-common-2025-09

such as want_digest, runtime_digest, 4k block is compared, etc.
- android_vh_handle_data_error
- android_vh_handle_metadata_error
Bug: 456302202
Signed-off-by: Tran Thi Hoai <hoai.tt5@samsung.corp-partner.google.com>
(cherry picked from commit ecb87057498e50242e0ef866b3532439f1205d4a)

drivers/android/vendor_hooks.c
@@ -75,6 +75,7 @@
 #include <trace/hooks/ogki_honor.h>
 #include <trace/hooks/suspend.h>
 #include <trace/hooks/gzvm.h>
+#include <trace/hooks/dmv_debug.h>
 
 /*
  * Export tracepoints that act as a bare tracehook (ie: have no trace event
@@ -582,3 +583,9 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_gzvm_destroy_vm_post_process);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_looper_state_registered);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_thread_release);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_read_done);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_handle_add_fec_mismatch_blks);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_handle_data_error);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_handle_metadata_error);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_handle_add_skipped_blks);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_handle_add_blks_map);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_handle_get_b_info);